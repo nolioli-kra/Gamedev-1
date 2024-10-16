ff4495f80188446b98abe2415f40b4777f5887d4
Nolioli <nolan.burgess01@gmail.com>
2024-10-16 01:53:47 -0600

    Matching Game - LAB
    added sound and ui scoring events.

--- Unity Events Practice/Assets/Scripts/Matching_Scripts/IntData.cs
+++ Unity Events Practice/Assets/Scripts/Matching_Scripts/IntData.cs
@@ -1,9 +1,13 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class IntData : ScriptableObject
 {
     public int intValue;
+
+    public UnityEvent disableEvent;
 
     public void UpdateValue(int num)
     {
@@ -11,9 +15,30 @@ public class IntData : ScriptableObject
         //Debug.Log("score is currently: " + intValue);
     }
 
+    public void SetValue(IntData obj)
+    {
+        intValue = obj.intValue;
+    }
+
+    public void CompareValue(IntData obj)
+    {
+        if (intValue >= obj.intValue)
+        {
+            
+        } else
+        {
+            intValue = obj.intValue;
+        }
+    }
+
     public void ResetValue(int num)
     {
         intValue = num;
         //Debug.Log("score reset");
+    }
+
+    private void OnDisable()
+    {
+         disableEvent.Invoke();
     }
 }
