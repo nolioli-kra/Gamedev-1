8fe543216d386561a70fb0e225c402cfa1d82d9f
Nolioli <nolan.burgess01@gmail.com>
2024-10-22 12:31:44 -0600

    Matching Game - Challenge
    The score now resets upon using the reset button and the current circles on screen now disappear when a mismatch is made.

--- Unity Events Practice/Assets/Scripts/Matching_Scripts/DestroyBehavior.cs
+++ Unity Events Practice/Assets/Scripts/Matching_Scripts/DestroyBehavior.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class DestroyBehavior : MonoBehaviour
 {
-    public float seconds = 1;
+    public float seconds = 1f;
+    public float checkInterval = 0.5f;
     private WaitForSeconds wfsObj;
 
+    public void Destroy()
+    {
+        StartCoroutine(DestroyRoutine());
+    }
+
+    public void DestroyTagRepeating()
+    {
+        InvokeRepeating(nameof(DestroyWithTag), checkInterval, checkInterval);
+    }
+    public void DestroyWithTag()
+    {
+        GameObject[] cylinders = GameObject.FindGameObjectsWithTag("Cylinders");
+
+        StartCoroutine(DestroyCylindersWithDelay(cylinders));
+    }
+    
     private IEnumerator DestroyRoutine()
     {
         wfsObj = new WaitForSeconds(seconds);
         yield return wfsObj;
         Destroy(gameObject);
     }
-
-    public void Destroy()
+    
+    IEnumerator DestroyCylindersWithDelay(GameObject[] cylinders)
     {
-        wfsObj = new WaitForSeconds(seconds);
-        Destroy(gameObject);
+        yield return new WaitForSeconds(seconds);
+        foreach (GameObject cylinder in cylinders)
+        {
+            Destroy(cylinder);
+        }
     }
 }
